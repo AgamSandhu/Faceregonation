5e7fb2acc7e56093b605a4c4ed9bd5154bdafefc
AgamSandhu <83796392+AgamSandhu@users.noreply.github.com>
2022-08-24 19:52:51 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,23 @@ function take_snapshot(){
 
 console.log('ml5 version',ml5.version); 
 
-classify=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-EORlraO2/model.jason',modelloaded)
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/WNaI6pD-z/model.json',modelloaded) 
 
 function modelloaded() {
     console.log("model loaded"); 
+}
+
+function gotresult(error,results) {
+    if(error){
+        console.error(error); 
+    }else{
+        console.log(results);
+        document.getElementById("expresstion_name").innerHTML=results[0].label; 
+        document.getElementById("expresstion_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
+
+function check(){
+    img=document.getElementById('capture_image');
+    classifier.classify(img,gotresult); 
 }
